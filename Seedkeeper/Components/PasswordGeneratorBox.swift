6fb4190ba8cc7ee2deccccf23829ab4a4cb9f7f4
lionelDELVAUX <36716936+lionelDELVAUX@users.noreply.github.com>
2024-07-29 16:54:15 +0200

    44: Memorable passwords

--- Seedkeeper/Components/PasswordGeneratorBox.swift
+++ Seedkeeper/Components/PasswordGeneratorBox.swift
@@ -14,9 +14,10 @@ class PasswordOptions: ObservableObject {
     @Published var includeUppercase: Bool = false
     @Published var includeNumbers: Bool = false
     @Published var includeSymbols: Bool = false
+    @Published var isMemorablePassword: Bool = false
     
     func userSelectedAtLeastOneIncludeOption() -> Bool {
-        return includeLowercase || includeUppercase || includeNumbers || includeSymbols
+        return includeLowercase || includeUppercase || includeNumbers || includeSymbols || isMemorablePassword
     }
 }
 
@@ -25,7 +26,7 @@ struct PasswordGeneratorBox: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            Text("Password lenght")
+            Text(String(localized: "passwordLength"))
                 .foregroundColor(.white)
             
             Slider(value: $options.passwordLength, in: 8...16, step: 1)
@@ -34,57 +35,100 @@ struct PasswordGeneratorBox: View {
             SatoText(text: String(format: "%.0f", options.passwordLength), style: .lightSubtitle)
                 .frame(maxWidth: .infinity, alignment: .center)
                 .padding(0)
-            
-            HStack() {
-                VStack {
-                    HStack {
-                        Spacer()
-                        Toggle("", isOn: $options.includeLowercase)
-                        Spacer()
+            if !options.isMemorablePassword {
+                HStack() {
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Toggle("", isOn: $options.includeLowercase)
+                            Spacer()
+                        }
+                        
+                        SatoText(text: "abc", style: .lightSubtitle)
+                            .frame(maxWidth: .infinity, alignment: .center)
                     }
                     
-                    SatoText(text: "abc", style: .lightSubtitle)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                }
-                
-                VStack {
-                    HStack {
-                        Spacer()
-                        Toggle("", isOn: $options.includeUppercase)
-                        Spacer()
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Toggle("", isOn: $options.includeUppercase)
+                            Spacer()
+                        }
+                        
+                        SatoText(text: "ABC", style: .lightSubtitle)
+                            .frame(maxWidth: .infinity, alignment: .center)
                     }
                     
-                    SatoText(text: "ABC", style: .lightSubtitle)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                }
-                
-                VStack {
-                    HStack {
-                        Spacer()
-                        Toggle("", isOn: $options.includeNumbers)
-                        Spacer()
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Toggle("", isOn: $options.includeNumbers)
+                            Spacer()
+                        }
+                        
+                        SatoText(text: "123", style: .lightSubtitle)
+                            .frame(maxWidth: .infinity, alignment: .center)
                     }
                     
-                    SatoText(text: "123", style: .lightSubtitle)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                }
-                
-                VStack {
-                    HStack {
-                        Spacer()
-                        Toggle("", isOn: $options.includeSymbols)
-                        Spacer()
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Toggle("", isOn: $options.includeSymbols)
+                            Spacer()
+                        }
+                        
+                        SatoText(text: "$#!", style: .lightSubtitle)
+                            .frame(maxWidth: .infinity, alignment: .center)
                     }
                     
-                    SatoText(text: "$#!", style: .lightSubtitle)
-                        .frame(maxWidth: .infinity, alignment: .center)
                 }
+                .toggleStyle(SwitchToggleStyle(tint: .white))
+                .foregroundColor(.white)
             }
-            .toggleStyle(SwitchToggleStyle(tint: .white))
-            .foregroundColor(.white)
+            
+            Spacer()
+                .frame(height: 6)
+            
+            HStack {
+                SatoToggle(isOn: $options.isMemorablePassword, label: "memorablePassword")
+                    .padding(0)
+            }
+            .padding(0)
         }
         .padding(20)
         .background(Colors.purpleBtn.opacity(0.5))
         .cornerRadius(20)
+    }
+}
+
+struct SatoToggle: View {
+    @Binding var isOn: Bool
+    var label: String
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            ZStack {
+                Circle()
+                    .strokeBorder(Color.white, lineWidth: 2)
+                    .background(isOn ? Colors.ledGreen : .clear)
+                    .clipShape(Circle())
+                    .frame(width: 28, height: 28)
+            }
+            .onTapGesture {
+                self.isOn.toggle()
+            }
+            
+            Spacer()
+                .frame(width: 18)
+            
+            SatoText(text: label, style: .lightSubtitle)
+                .frame(height: 38)
+                .lineLimit(nil)
+            
+            Spacer()
+        }
+        .padding()
     }
 }
